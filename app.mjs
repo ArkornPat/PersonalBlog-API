6ddf355b31f6e081606704e254278c509c6b31ec
Arkorn Pattanavibul <arkorn.pat@gmail.com>
2024-11-07 13:35:58 +0700

    setup vercel

--- app.mjs
+++ app.mjs
@@ -1,13 +1,15 @@
 import express from "express";
+import cors from "cors";
 
 const app = express();
-const port = 4001;
+const port = process.env.PORT || 4001;
 
+app.use(cors());
 app.use(express.json());
 
 const profileData = {
   name: "john",
-  age: 20
+  age: 20,
 };
 
 //app.method(endpoint_path, callback)
